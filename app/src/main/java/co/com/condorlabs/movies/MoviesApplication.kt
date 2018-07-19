3bde24fdf51498d52d16c35d46aba7e463698c0a
Luis David Goyes <lgoyes@condorlabs.io>
2018-07-19 11:12:30 -0500

    Finished attaching lifecycle to presenters

--- app/src/main/java/co/com/condorlabs/movies/MoviesApplication.kt
+++ app/src/main/java/co/com/condorlabs/movies/MoviesApplication.kt
@@ -3,6 +3,7 @@ package co.com.condorlabs.movies
 import android.app.Application
 import co.com.condorlabs.movies.di.ApplicationComponent
 import co.com.condorlabs.movies.di.ApplicationModule
+import co.com.condorlabs.movies.di.DaggerApplicationComponent
 
 /**
  * @author Luis Goyes (lgoyes@condorlabs.io) on July/19/2018
