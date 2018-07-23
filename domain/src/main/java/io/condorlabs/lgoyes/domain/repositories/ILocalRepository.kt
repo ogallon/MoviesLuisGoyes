230930250b744fb82a0ace5fc83790eb88327ef2
Luis David Goyes <lgoyes@condorlabs.io>
2018-07-23 17:32:38 -0500

    Temporal advance

--- domain/src/main/java/io/condorlabs/lgoyes/domain/repositories/ILocalRepository.kt
+++ domain/src/main/java/io/condorlabs/lgoyes/domain/repositories/ILocalRepository.kt
@@ -1,9 +1,11 @@
 package io.condorlabs.lgoyes.domain.repositories
 
+import io.reactivex.Completable
+
 
 /**
  * @author Luis Goyes (lgoyes@condorlabs.io) on July/23/2018
  */
 interface ILocalRepository {
-
+    fun insertMovie( movie : DomainDBMovie ): Completable
 }
